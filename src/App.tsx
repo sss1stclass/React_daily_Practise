0835c73251a0aced9de43e52d8873e0f2717f738
sss1stclass <sohilkr88@gmail.com>
2024-11-28 15:03:30 +0530

    day-3 prop passing

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,15 @@ import { createContext, useState } from 'react'
 // import FormData from './components/FormData';
 import { React1CheckBox } from './components/React1CheckBox';
 import TestComponent from './components/TestComponent';
+import Day2_Prop from './components/Day2_Prop';
 import Day1 from './components/Day1';
 import { Box, Typography } from '@mui/material';
+
+const obj = {
+  name:'PuzzledBird',
+  Profession:'Frontend Developer',
+  Experience:'2+Years'
+}
 
 const addFunctionality = createContext<any>('');
 function App() {
@@ -29,6 +36,10 @@ function App() {
       </Box>
       <Box mb={2} sx={{ border: '2px solid black' }}>
         <Typography variant='body1'>Day-2</Typography>
+        <Day2_Prop  details= {obj}/>
+      </Box>
+      <Box mb={2} sx={{ border: '2px solid black' }}>
+        <Typography variant='body1'>Day-3</Typography>
         <React1CheckBox />
       </Box>
     </Box>
---
+++ src/components/Day2_Prop.tsx
@@ -0,0 +1,26 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+const Day2_Prop = ({details}:any) => {
+ 
+    const {name, Profession, Experience} = details;
+            
+  return (
+    // by using object destructuring...
+    
+    <Box display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
+    <Typography variant='h5'>Hello, {name}</Typography>
+    <Typography variant='h5'>{Profession}</Typography>
+    <Typography variant='h5'>{Experience}</Typography>
+     
+     {/* another approach for using props  */}
+
+    {/* <Typography variant='h5'>Hello, {details.name}</Typography>
+    <Typography variant='h5'>{details.Profession}</Typography>
+    <Typography variant='h5'>{details.Experience}</Typography> */}
+    </Box>
+
+  )
+}
+
+export default Day2_Prop
