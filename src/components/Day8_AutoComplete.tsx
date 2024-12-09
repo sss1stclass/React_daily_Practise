d5afaf1746d8d98f01db2ceba58dcb6de421ad9a
sss1stclass <sohilkr88@gmail.com>
2024-12-09 21:39:55 +0530

    update code

--- src/components/Day8_AutoComplete.tsx
+++ src/components/Day8_AutoComplete.tsx
@@ -1,86 +1,77 @@
-import { Box, Divider, TextField, Typography } from "@mui/material";
+import { Box, Divider, Paper, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
 const dataSet = [
-    {
-        name: 'sohil',
-        id: 1
-    },
-    {
-        name: 'mohil',
-        id: 2
-    },
-    {
-        name: 'rohit',
-        id: 3
-    },
-    {
-        name: 'vivek',
-        id: 4
-    },
-    {
-        name: 'aman',
-        id: 5
-    },
-
-]
+    { "name": "Olivia", "id": 1 },
+    { "name": "Charlie", "id": 2 },
+    { "name": "Hannah", "id": 3 },
+    { "name": "Ian", "id": 4 },
+    { "name": "Mia", "id": 5 },
+    { "name": "Alice", "id": 6 },
+    { "name": "Sophia", "id": 7 },
+    { "name": "Paul", "id": 8 },
+    { "name": "Violet", "id": 9 },
+    { "name": "Noah", "id": 10 }
+  ];
 
 const Day8_AutoComplete = () => {
+  const [data, setData] = useState<string>("");
+  const [showModal, setShowModal] = useState(false);
+  const [inputData, setInputData] = useState<any>(dataSet);
 
-    const [data, setData] = useState<any>('sohil')
-    const handleChange = (e: any) => {
-        setData(e.target.value);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setData(e.target.value);
+  };
+
+  const dataChange = () => {
+    if (data.length > 1) {
+      const filterData = dataSet.filter((item: any) =>
+        item.name.toLowerCase().includes(data.toLowerCase())
+      );
+      setInputData(filterData);
+    } else {
+      setInputData(dataSet);
     }
-    const [showModal, setShowModal] = useState(false);
+  };
 
-    const [inputData, setInputData] = useState<any>(dataSet);
+  useEffect(() => {
+    dataChange();
+  }, [data]);
 
-    const dataChange = () => {
-        if(data.length >1){
-            const filterData = inputData.filter((item: any) => item.name == data);
-            console.log(filterData);
-            setInputData(filterData);
-        }
-        else{
-            setInputData(dataSet);
-        }
-
-    }
-
-    useEffect(()=>{
-      dataChange();
-    },[data])
-
-
-    return (
-        <Box display='flex' flexDirection='column' alignItems='center' justifyContent='center' p={2}>
-            <TextField
-                name='autocomplete'
-                label='Search Name'
-                placeholder='Type to search your name'
-                variant="outlined"
-                value={data}
-                onChange={handleChange}
-                onFocus={() => setShowModal(true)}
-            />
-            {
-                showModal && (
-                    <Box sx={{ border: '2px solid black' }} p={3}>
-                        {
-                            inputData.map((item: any) => {
-                                return (
-                                    <Typography key={item.id}>
-                                        {item.name}
-                                        <Divider />
-                                    </Typography>
-                                )
-                            })
-                        }
-                    </Box>
-                )
-            }
-        </Box>
-    )
-}
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" p={2}>
+      <TextField
+        name="autocomplete"
+        label="Search Name"
+        placeholder="Type to search your name"
+        variant="outlined"
+        value={data}
+        onChange={handleChange}
+        onFocus={() => setShowModal(true)}
+        onBlur={() => setShowModal(false)} 
+      />
+      {showModal && (
+        <Paper
+          elevation={3}
+          sx={{
+            width: "200px",
+            maxHeight: "150px",
+            overflowY: "auto",
+            mt: 2,
+            border: "1px solid #ccc",
+            padding: "8px",
+          }}
+        >
+          {inputData.map((item: any) => (
+            <Box key={item.id}>
+              <Typography>{item.name}</Typography>
+              <Divider />
+            </Box>
+          ))}
+        </Paper>
+      )}
+    </Box>
+  );
+};
 
 export default Day8_AutoComplete;
