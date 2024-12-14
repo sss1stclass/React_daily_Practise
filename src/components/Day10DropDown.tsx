6a06c4e2601fd99ccd43dbc77260f29460c49ef6
sss1stclass <sohilkr88@gmail.com>
2024-12-14 19:04:32 +0530

    refactored code with gpt

--- src/components/Day10DropDown.tsx
+++ src/components/Day10DropDown.tsx
@@ -1,60 +1,77 @@
-import { Box, Divider, Paper, TextField, Typography } from "@mui/material"
-import { useState } from "react"
+import { Box, Divider, Paper, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
-const list = ['sohil', 'aman', 'raj', 'vivek', 'sonal']
 
+const options = ["sohil", "aman", "raj", "vivek", "sonal"];
 
 const Day10DropDown = () => {
-    const [name, setName] = useState<any>('');
+    const [name, setName] = useState<string>("");
     const [toggle, setToggel] = useState<boolean>(false);
+    const [isFocused, setIsFocused] = useState<boolean>(false);
 
-    const handleSelect = (item: any) => {
-        const filterData = list.filter((data: any) => data == item)
-        setName(filterData);
-    }
+    const handleSelect = (item: string) => {
+        setName(item);
+        setToggel(false);
+    };
+
+    const handleBlur = () => {
+        setTimeout(() => {
+            if (!isFocused) setToggel(false);
+        }, 150);
+    };
 
     return (
-        <Box display='flex' flexDirection='column' alignItems='center' justifyContent='center' p={2}>
-            <Typography mb={2} variant="h4">The selected name is <span style={{ color: 'Blue', textTransform: 'capitalize' }}> {name}</span></Typography>
+        <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" p={2}>
+            <Typography mb={2} variant="h4">
+                The selected name is <span style={{ color: "blue", textTransform: "capitalize" }}>{name}</span>
+            </Typography>
             <TextField
                 variant="outlined"
-                label='Select Name'
-                name='selectName'
+                label="Select Name"
+                name="selectName"
                 value={name}
                 onFocus={() => setToggel(true)}
-                onBlur={() => setToggel(false)}
+                onBlur={handleBlur}
             />
             {toggle && (
                 <Paper
                     elevation={3}
-                    onMouseDown={(e: any) => e.preventDefault()}
                     sx={{
-                        maxHeight: '150px',
-                        width: '200px',
-                        marginTop: '10px',
-                        overflowY: 'auto',
-                        border: '1px dashed black'
+                        maxHeight: "150px",
+                        width: "100%",
+                        marginTop: "5px",
+                        overflowY: "auto",
+                        border: "1px solid #ddd",
+                        borderRadius: "4px",
                     }}
                 >
-                    {
-                        list.map((item: any) => {
-                            return (
-                                <Box>
-                                    <Typography component='button' onClick={() => handleSelect(item)} textAlign='center' textTransform='capitalize'>{item}</Typography>
-                                    <Divider />
-                                </Box>
-                            )
-                        })
-                    }
-
+                    {options.map((item, index) => (
+                        <Box key={index} onMouseDown={() => setIsFocused(true)} onMouseUp={() => setIsFocused(false)}>
+                            <Typography
+                                component="button"
+                                onClick={() => handleSelect(item)}
+                                textAlign="center"
+                                textTransform="capitalize"
+                                sx={{
+                                    display: "block",
+                                    width: "100%",
+                                    padding: "8px",
+                                    textAlign: "left",
+                                    border: "none",
+                                    background: "none",
+                                    cursor: "pointer",
+                                    "&:hover": { backgroundColor: "#f0f0f0" },
+                                }}
+                            >
+                                {item}
+                            </Typography>
+                            <Divider />
+                        </Box>
+                    ))}
                 </Paper>
             )}
-
-
-
-
         </Box>
-    )
-}
+    );
+};
 
-export default Day10DropDown
+export default Day10DropDown;
