6990a41a5dee75ca888fcb312674586c0218564a
sss1stclass <sohilkr88@gmail.com>
2025-01-01 21:42:12 +0530

    accordion completed

--- src/components/Day14Accordian.tsx
+++ src/components/Day14Accordian.tsx
@@ -1,8 +1,72 @@
-import { Box } from '@mui/material'
+import { Box, IconButton, Paper, Typography } from '@mui/material'
+import { useState } from 'react'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+const data = [
+  {
+    Name: 'Accordian 1',
+    id: 1,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquam, blanditiis eligendi dolores dolorum quas',
+    show: false,
+  },
+  {
+    Name: 'Accordian 2',
+    id: 2,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquam, blanditiis eligendi dolores dolorum quas',
+    show: false,
+  },
+  {
+    Name: 'Accordian 3',
+    id: 3,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquam, blanditiis eligendi dolores dolorum quas',
+    show: false,
+  },
+]
 
 const Day14Accordian = () => {
+  const [showData, setShowData] = useState<any>(data);
+
+  const handleShow = (id: number) => {
+    setShowData((prevData: any) =>
+      prevData.map((item: any) => {
+        if (item.id === id) {
+          return { ...item, show: !item.show }
+        } else {
+          return item;
+        }
+      }
+
+      )
+    );
+  };
+
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" p={2}></Box>
+    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" p={2}>
+      {
+        showData.map((item: any) => {
+          return (
+            <Paper key={item.id} sx={{ display: 'flex', width: '100%', flexDirection: 'column', padding: 1, marginBottom: 1, transition: 'ease-in-out' }}>
+              <Box sx={{ display: 'flex', width: '100%', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Typography fontWeight={600}>{item?.Name}</Typography>
+                <IconButton onClick={() => handleShow(item.id)}>
+                  <KeyboardArrowDownIcon sx={{ fontWeight: '600', color: 'black' }} />
+                </IconButton>
+              </Box>
+              <Box sx={{ transition: 'ease-in-out' }}>
+                {
+                  item.show && (
+                    <Box width='100%'>
+                      {item.text}
+                    </Box>
+                  )
+                }
+              </Box>
+            </Paper>
+
+          )
+        })
+      }
+    </Box>
   )
 }
 
